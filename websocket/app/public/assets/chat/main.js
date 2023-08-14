c6379e22e9f44a7fbde629646dc46366c49d54bd
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-08-14 23:50:16 +0600

    refactor(auto connection): Auto connection when page loading

--- websocket/app/public/assets/chat/main.js
+++ websocket/app/public/assets/chat/main.js
@@ -2,6 +2,7 @@ import SocketHandler from './SocketHandler.js';
 import store from './store.js';
 import ui from './ui.js';
 
+const socket = SocketHandler.connectToSocketIOServer()
 
 
 const nameinput = document.querySelector('#name_input');
@@ -12,7 +13,6 @@ nameinput.addEventListener('keyup', (event) =>{
 const newmessageinput = document.querySelector('#new_message_input');
 const newmessagesubmit = document.querySelector('#new_message_submit');
 newmessagesubmit.addEventListener('click', (event) =>{
-    const socket = SocketHandler.connectToSocketIOServer()
     let message = newmessageinput.value
     SocketHandler.sendGroupMessage(socket, message)
 })
