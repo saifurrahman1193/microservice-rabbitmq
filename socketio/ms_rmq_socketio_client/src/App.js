b6c60a8d833251e7b38502cd4c6ea3a21f545a5f
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-08-17 23:14:45 +0600

    feat(absolute path): Configuring Absolute path in React Projects

--- socketio/ms_rmq_socketio_client/src/App.js
+++ socketio/ms_rmq_socketio_client/src/App.js
@@ -1,50 +1,10 @@
-import { useEffect, useState } from 'react';
-import Button from '@mui/material/Button';
-import io from 'socket.io-client';
-import { Box, TextField, Container, Typography } from '@mui/material';
+import ChatWindow from 'src/components/ChatWindow';
 
 function App() {
-    const [socket, setSocket] = useState(null);
-    const [message, setMessage] = useState("");
-    const [chat, setChat] = useState([]);
-
-    const handleForm = (e) => {
-        e.preventDefault();
-        socket.emit("send-message", {message: message});
-        setMessage("")
-    }
-
-    useEffect(() => {
-        setSocket(io("http://localhost:803"));
-    }, []);
-
-    useEffect(() => {
-        if(!socket) return;
-        socket.on("send-message-back", (data)=>{
-            setChat((prev) => [...prev, data?.message]);
-        })
-    }, [socket]);
+    
   return (
     <div>
-        <Container>
-            <Box sx={{ marginBottom:5 }}>
-                {
-                    chat?.map((msg) =>
-                        <Typography >{msg}</Typography>
-                    )
-                }
-            </Box>
-            <Box component="form" onSubmit={handleForm}>
-                <TextField
-                id="message"
-                label="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                size='small'
-                />
-                <Button variant='text' type='submit'>Send</Button>
-            </Box>
-        </Container>
+        <ChatWindow />
     </div>
   );
 }
---
+++ socketio/ms_rmq_socketio_client/src/components/ChatWindow.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { Box, TextField, Container, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import io from 'socket.io-client';
+
+function ChatWindow() {
+    const [socket, setSocket] = useState(null);
+    const [message, setMessage] = useState("");
+    const [chat, setChat] = useState([]);
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        socket.emit("send-message", {message: message});
+        setMessage("")
+    }
+
+    useEffect(() => {
+        setSocket(io("http://localhost:803"));
+    }, []);
+
+    useEffect(() => {
+        if(!socket) return;
+        socket.on("send-message-back", (data)=>{
+            setChat((prev) => [...prev, data?.message]);
+        })
+    }, [socket]);
+
+
+  return (
+        <Container>
+            <Box sx={{ marginBottom:5 }}>
+                {
+                    chat?.map((msg) =>
+                        <Typography >{msg}</Typography>
+                    )
+                }
+            </Box>
+            <Box component="form" onSubmit={handleForm}>
+                <TextField
+                id="message"
+                label="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                size='small'
+                />
+                <Button variant='text' type='submit'>Send</Button>
+            </Box>
+        </Container>
+  )
+}
+
+export default ChatWindow
