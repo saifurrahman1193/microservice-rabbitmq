5b0c6a030f50ba196fedb7fbb36b7832e03745cf
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-08-20 23:26:36 +0600

    feat(mongodb): adding mongodb

--- socketio/ms_rmq_socketio_server/index.js
+++ socketio/ms_rmq_socketio_server/index.js
@@ -5,6 +5,20 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import sockets from './socket/sockets.js';
+import mongoose from 'mongoose';
+
+// await mongoose.connect('mongodb://localhost:27017/chat_app', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'Connection error:'));
+// db.once('open', () => {
+//     console.log('Connected to the database');
+// });
+
 
 const app = express();
 dotenv.config();
