2720ba1018f230ee1a764782359a49a075c76089
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-08-21 23:27:21 +0600

    refactor(mongodb): dynamic room

--- socketio/ms_rmq_socketio_client/src/App.js
+++ socketio/ms_rmq_socketio_client/src/App.js
@@ -12,7 +12,7 @@ function App() {
 
     return (
         <>
-            <Header  context={socket} />
+            <Header  socket={socket} />
             <Outlet context={{ socket }} />
         </>
     );
