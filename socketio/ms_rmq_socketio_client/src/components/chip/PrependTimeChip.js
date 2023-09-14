669fde287c117e445f4cebb55cea820e6b97c441
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-09-14 16:46:02 +0600

    fix(react crud): components refactoring

--- socketio/ms_rmq_socketio_client/src/components/chip/PrependTimeChip.js
+++ socketio/ms_rmq_socketio_client/src/components/chip/PrependTimeChip.js
@@ -1,9 +1,8 @@
-import {  Chip, Avatar } from '@mui/material';
+import {  Chip } from '@mui/material';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 function PrependTimeChip(props) {
     const {children, ...domProps} = props;
-    console.log(domProps);
     return (
         <Chip avatar={<AccessTimeIcon fontSize='small'  />}  {...domProps} />
     )
