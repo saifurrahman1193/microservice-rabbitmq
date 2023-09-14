2deca1cda6b92a5a3e7b9891c36a0cd63e97df6a
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-09-14 15:14:55 +0600

    fix(react crud): components refactoring

--- socketio/ms_rmq_socketio_client/src/components/alert/timeout-alert/useAlert.js
+++ socketio/ms_rmq_socketio_client/src/components/alert/timeout-alert/useAlert.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Alert } from '@mui/material';
 import './style.css';
-import PrivacyTipOutlinedIcon from '@mui/icons-material/PrivacyTipOutlined';
-import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined';
 import WarningAmberOutlinedIcon from '@mui/icons-material/WarningAmberOutlined';
+import PrivacyTipOutlinedIcon from '@mui/icons-material/PrivacyTipOutlined';
 
 export const useAlert = () => {
 
@@ -48,24 +48,24 @@ export const useAlert = () => {
 
     const getAlertStyle = (type = 'success') => {
         switch (type) {
-            case 'error':
-                return { backgroundColor: 'rgba(220,53,69,.85)', color: 'white' };
-            case 'warning':
-                return { backgroundColor: 'rgba(255,193,7,.85)', color: 'black' };
             case 'info':
                 return { backgroundColor: 'rgba(23,162,184,.85)', color: 'white' };
             case 'success':
                 return { backgroundColor: 'rgba(40,167,69,.85)', color: 'white' };
+            case 'warning':
+                return { backgroundColor: 'rgba(255,193,7,.85)', color: 'black' };
+            case 'error':
+                return { backgroundColor: 'rgba(220,53,69,.85)', color: 'white' };
             default:
                 return { backgroundColor: 'rgba(40,167,69,.85)', color: 'white' }; // Default style
         }
     };
 
     const iconComponentMap = {
-        'success': CheckCircleOutlineOutlinedIcon,
-        'error': PrivacyTipOutlinedIcon,
         'info': InfoOutlinedIcon,
+        'success': CheckCircleOutlineOutlinedIcon,
         'warning': WarningAmberOutlinedIcon,
+        'error': PrivacyTipOutlinedIcon,
     };
     function IconDynamicComponent(props) {
         const SelectedComponent = iconComponentMap[alertData?.type] || CheckCircleOutlineOutlinedIcon;
