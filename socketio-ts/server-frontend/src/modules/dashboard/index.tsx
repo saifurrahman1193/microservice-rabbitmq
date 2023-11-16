0ac402345f9a15f6ad739f51d3a50cdf3afba33a
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-11-16 22:41:22 +0600

    feat(layout): layout done

--- socketio-ts/server-frontend/src/modules/dashboard/index.tsx
+++ socketio-ts/server-frontend/src/modules/dashboard/index.tsx
@@ -5,9 +5,8 @@ type Props = {}
 const Dashboard = (props: Props) => {
     return (
         <>
-            <div className="p-4 sm:ml-64">
+            <div className="min-h-screen p-4 sm:ml-64">
                 <div className="p-3 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-
                     <div className="flex items-center justify-center h-48  rounded bg-gray-50 dark:bg-gray-800">
                         <p className="text-2xl text-gray-400 dark:text-gray-500">
                             Welcome to socket.io server
@@ -15,10 +14,7 @@ const Dashboard = (props: Props) => {
                     </div>
                 </div>
             </div>
-
-           
         </>
-
     )
 }
 
