4c5c6c74ee19b0795e271b5e8a0054ab8bcaa259
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-11-16 22:24:40 +0600

    feat(layout): layout done

--- socketio-ts/server-frontend/src/components/footer/index.tsx
+++ socketio-ts/server-frontend/src/components/footer/index.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 const Footer = (props: Props) => {
     return (
-        <footer className="bg-gray-100 rounded-lg shadow dark:bg-gray-100">
-            <div className="w-full max-w-screen-xl p-4 md:py-8">
-                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Open Source</a>. No Rights Reserved.</span>
+        <footer className="bottom-0 left-0 z-20 w-full bg-white border-t border-gray-100 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+            <div className="w-full max-w-screen-xl p-3 md:py-8">
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()} <a href="https://github.com/saifurrahman1193" className="hover:underline">Open Source</a>. No Rights Reserved.</span>
             </div>
         </footer>
     )
