942224f02cfc74cbe212f9b590bbafc24716a29d
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-11-25 23:41:11 +0600

    refactor(middleware): authenticated middleware added

---
+++ socketio-ts/server-backend/src/controller/UserController.ts
@@ -0,0 +1,51 @@
+import {Request, Response} from 'express';
+import { deleteUserById, getUsers, getUserById } from '../model/User';
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await getUsers();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    if (!user) {
+        return res.sendStatus(400);
+    }
+    
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
--- socketio-ts/server-backend/src/route/user.ts
+++ socketio-ts/server-backend/src/route/user.ts
@@ -1,10 +1,11 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
-import { getAllUsers, deleteUser, updateUser } from '../controllers/user';
-import isOwner from '../middlewares';
+import { getAllUsers, deleteUser, updateUser } from '../controller/UserController';
+import isAuthenticated from '../middleware/IsAuthenticatedMiddlware';
+import isOwner from '../middleware/IsOwnerMiddleware';
 
 export default (router: Router) => {
-  router.get('/users', isAuthenticated, getAllUsers);
-  router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
-  router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
+    router.get('/users', isAuthenticated, getAllUsers);
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 };
