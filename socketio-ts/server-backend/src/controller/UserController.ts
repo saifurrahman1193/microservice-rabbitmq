51ca1f1d0fb3024b9b98a045451a3dcac4de7cc2
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-11-28 22:58:13 +0600

    feat(user): more user api added

--- socketio-ts/server-backend/src/controller/UserController.ts
+++ socketio-ts/server-backend/src/controller/UserController.ts
@@ -1,51 +1,83 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import { deleteUserById, getUsers, getUserById } from '../model/User';
+import { get } from 'lodash';
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await getUsers();
+    try {
+        const users = await getUsers();
 
-    return res.status(200).json(users);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
+        return res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const deletedUser = await deleteUserById(id);
+        const deletedUser = await deleteUserById(id);
 
-    return res.json(deletedUser);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
+        return res.json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
 
 export const updateUser = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { username } = req.body;
+    try {
 
-    if (!username) {
-      return res.sendStatus(400);
-    }
+        const { id } = req.params;
+        const { name, username } = req.body;
 
-    const user = await getUserById(id);
+        if (!username) {
+            return res.sendStatus(400);
+        }
 
-    if (!user) {
+
+        const user = await getUserById(id);
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        user.username = username;
+        user.name = name;
+        await user.save();
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
         return res.sendStatus(400);
     }
-    
-    user.username = username;
-    await user.save();
+}
 
-    return res.status(200).json(user).end();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
+export const updateProfile = async (req: Request, res: Response) => {
+    try {
+
+        const { name, username } = req.body;
+
+        if (!username) {
+            return res.sendStatus(400);
+        }
+
+        const user_id = get(req, 'identity._id') || '';
+
+        const user = await getUserById(user_id);
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        user.username = username;
+        user.name = name;
+        await user.save();
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
--- socketio-ts/server-backend/src/model/User.ts
+++ socketio-ts/server-backend/src/model/User.ts
@@ -32,7 +32,7 @@ export const getUsers = () => User.find();
 export const getUserByEmail = (email: string) => User.findOne({ email });
 export const getUserByUserName = (username: string) => User.findOne({ username });
 export const getUserBySessionToken = (sessionToken: string) => User.findOne({ 'authentication.sessionToken': sessionToken });
-export const getUserById = (id: string) => User.findOne({ id });
+export const getUserById = (_id: string) => User.findOne({ _id });
 export const createUser = (values: Record<string, any>) => new User(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) => User.findByIdAndDelete({ _id: id });
 export const updateUserById = (id: string, values: Record<string, any>) => User.findByIdAndUpdate(id, values)
