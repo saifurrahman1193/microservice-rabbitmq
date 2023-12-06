499617c77fc53928c1f3c7b63eaf8f76300a6b16
saifurrahman1193 <saifur.rahman1193@gmail.com>
2023-12-06 14:41:33 +0600

    fix(body-parser): removed

--- socketio-ts/server-backend/src/server.ts
+++ socketio-ts/server-backend/src/server.ts
@@ -4,7 +4,6 @@ import { config } from './config/index';
 import userRoutes from './route/user';
 import authRoutes from './route/authentication';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import multer from 'multer';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -12,24 +11,21 @@ import LoggerMiddlware from './middleware/Logger';
 import { set_response } from './helper/APIResponser';
 
 
-
-
 const router = express();
 
 
 /** Only Start Server if Mongoose Connects */
 export const startServer = async () => {
+    router.use(express.urlencoded({ extended: true }));
     router.use(cors({ credentials: true }));
     router.use(compression());
     router.use(cookieParser());
-    router.use(bodyParser.json());
     // for parsing multipart/form-data
     var forms = multer();
     router.use(forms.array(undefined!));
 
     router.use(LoggerMiddlware); // for logging
 
-    router.use(express.urlencoded({ extended: true }));
     router.use(express.json());
 
     /** Rules of our API */
@@ -44,22 +40,21 @@ export const startServer = async () => {
         next();
     });
 
-    
+
     /** Routes */
     router.use('/api/user', userRoutes);
     router.use('/api/auth', authRoutes);
 
     /** Healthcheck */
-    router.get('/ping', (req, res, next) => 
+    router.get('/ping', (req, res, next) =>
         set_response(res, null, 200, 'success', ['Request successful'], null)
     );
 
-    
 
     /** Error handling */
     router.use((req, res, next) => {
         const error = new Error('Not found');
-        return set_response(res, null, 404, 'success', ['Not Found '+error.message], null);
+        return set_response(res, null, 404, 'success', ['Not Found ' + error.message], null);
     });
 
     http.createServer(router).listen(config.server.port, () => console.log(`Server is running on port ${config.server.port}`));
