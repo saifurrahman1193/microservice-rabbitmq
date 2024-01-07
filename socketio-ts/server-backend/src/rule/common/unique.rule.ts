dec6d6b1b99af1946f2b319557b36f7b5c105bac
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-01-08 00:25:50 +0600

    feat(validation): unique validation added

--- socketio-ts/server-backend/src/rule/common/unique.rule.ts
+++ socketio-ts/server-backend/src/rule/common/unique.rule.ts
@@ -1,25 +1,30 @@
-import { Document, Model } from 'mongoose';
-
 const unique = async (
-    model: any,
-    field: string,
-    value: any
+  model: any,
+  field: string,
+  value: any,
+  exceptField?: string,
+  exceptValue?: any
 ): Promise<number> => {
-    try {
-        // Build the filter object
-        const filter: any = {
-            [field]: value,
-        };
+  try {
+    // Build the filter object
+    const filter: any = {
+      [field]: value,
+    };
 
-        // Get the MongoDB document from the model
-        const existingDocument: any = await model.findOne(filter).exec();
-
-        // Return 1 if there are no matching documents, otherwise return 0
-        return existingDocument ? 0 : 1;
-    } catch (error) {
-        console.error('Error checking uniqueness in MongoDB:', error);
-        return 0; // Return 0 in case of an error
+    // If exceptField and exceptValue are provided, exclude them from the filter
+    if (exceptField && exceptValue) {
+      filter[exceptField] = { $ne: exceptValue };
     }
+
+    // Get the MongoDB document from the model
+    const existingDocument: any = await model.findOne(filter).exec();
+
+    // Return 1 if there are no matching documents, otherwise return 0
+    return existingDocument ? 0 : 1;
+  } catch (error) {
+    console.error('Error checking uniqueness in MongoDB:', error);
+    return 0; // Return 0 in case of an error
+  }
 };
 
 export { unique };
