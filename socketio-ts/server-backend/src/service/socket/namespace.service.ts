23bacd8dca08d1668b6b0016a3b88f9bd15ec9fb
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-04 17:59:50 +0600

    updated

---
+++ socketio-ts/server-backend/src/service/socket/namespace.service.ts
@@ -0,0 +1,12 @@
+import { User } from '../../model/authentication/user.model';
+
+const getAllNamespaces = () => 1;
+const getUserWiseAllNamespaces = () => 1;
+const getUserWiseActiveNamespaces = () => 1;
+
+export const userService = {
+    getAllNamespaces,
+    getUserWiseAllNamespaces,
+    getUserWiseActiveNamespaces,
+
+}
