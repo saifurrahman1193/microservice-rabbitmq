e22db05b48daa60246e523aa8fa3245343576d9b
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-05 23:28:04 +0600

    updated

--- socketio-ts/server-backend/src/service/socket/namespace.service.ts
+++ socketio-ts/server-backend/src/service/socket/namespace.service.ts
@@ -1,5 +1,7 @@
 import { Server, Socket } from 'socket.io';
 import { namespaceService as namespaceServiceDB } from '../app/namespace.service';
+import { roomService } from './room.service';
+roomService
 
 const processNamespace = async (io: Server) => {
     const namespace = io.of(/^\/\w+$/);  // Dynamic Namespace
@@ -15,7 +17,8 @@ const processNamespace = async (io: Server) => {
                 console.log(`A user connected to registered namespace: ${namespaceName}`);
                 // Handle connections within the registered namespace
 
-                
+                roomService.joinRoom(socket);
+
             } else {
                 console.log(`Connection attempt to unregistered namespace: ${namespaceName}`);
                 // Handle unauthorized connections or take appropriate action
--- socketio-ts/server-backend/src/service/socket/room.service.ts
+++ socketio-ts/server-backend/src/service/socket/room.service.ts
@@ -1,11 +1,14 @@
+import { Server, Socket } from 'socket.io';
 
-const getAllNamespaces = () => 1;
-const getUserWiseAllNamespaces = () => 1;
-const getUserWiseActiveNamespaces = () => 1;
+const joinRoom = (socket: Socket) => {
+        // Event listener for the 'joinRoom' event
+        socket.on('joinRoom', (roomName) => {
+            // Join the specified room
+            socket.join(roomName);
+            console.log(`User ${socket.id} joined room: ${roomName}`);
+        });
+};
 
-export const userService = {
-    getAllNamespaces,
-    getUserWiseAllNamespaces,
-    getUserWiseActiveNamespaces,
-
+export const roomService = {
+    joinRoom,
 }
