7d6391f70abd510b7c068ce716df0481534942ee
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-07 18:00:37 +0600

    updated

--- socketio-ts/server-backend/src/service/socket/room.service.ts
+++ socketio-ts/server-backend/src/service/socket/room.service.ts
@@ -6,7 +6,14 @@ const joinRoom = (socket: Socket) => {
             // Join the specified room
             socket.join(roomName);
             console.log(`User ${socket.id} joined room: ${roomName}`);
+
+            socket.to(roomName).emit('joinedRoom',  {user: socket.id, roomName: roomName})
         });
+
+        // console.log(Object.keys(io.sockets.adapter.rooms));
+
+        // Emit 'joinedRoom' event to the specific room
+        // io.sockets.in(roomName).emit('joinedRoom', `User ${socket.id} has joined room ${roomName}`);
 };
 
 export const roomService = {
