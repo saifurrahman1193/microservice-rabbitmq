74b7e2dee415b32dc12374bcb3bf5f8253ae8b4a
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-10 17:26:44 +0600

    updated

--- socketio-ts/server-backend/src/service/socket/index.ts
+++ socketio-ts/server-backend/src/service/socket/index.ts
@@ -1,14 +1,12 @@
-import express from 'express';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 import { namespaceService } from './namespace.service';
-const app = express();
 
 
 /** Only Socket Server if Mongoose Connects */
-export const setupSocketServer = async (app_server: any) => {
+export const setupSocketServer = async (express_server: any) => {
 
     // Socket.io server configuration
-    const io = new Server(app_server, {
+    const io = new Server(express_server, {
         cors: {
             origin: ["*"], // Replace with your frontend URL
             // methods: ["GET", "POST"],
--- socketio-ts/server-backend/src/service/socket/namespace.service.ts
+++ socketio-ts/server-backend/src/service/socket/namespace.service.ts
@@ -18,8 +18,8 @@ const processNamespace = async (io: Server) => {
                 // console.log(`A user connected to registered namespace: ${namespaceName}`);
                 // Handle connections within the registered namespace
 
-                // roomService.joinRoom(socket);  // joining to a room
-                roomService.joinRooms(socket);  // joining to a room
+                roomService.joinRoom(socket);  // joining to a room
+                roomService.joinRooms(socket);  // joining to multiple room
             } else {
                 console.log(`Connection attempt to unregistered namespace: ${namespaceName}`);
                 // Handle unauthorized connections or take appropriate action
