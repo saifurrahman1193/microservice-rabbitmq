e7cdcf78117bd3f6e461369e3daa56dcc3f53007
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-12 17:49:31 +0600

    refactor(event): event refactoring

--- socketio-ts/server-backend/src/service/socket/singlechat.service.ts
+++ socketio-ts/server-backend/src/service/socket/singlechat.service.ts
@@ -2,12 +2,13 @@ import { Socket } from 'socket.io';
 
 // event
 const sendMessageProcess = (socket: Socket) => {
-    socket.on('single-chat/send-message', (params: { message: string, target_socket_id: string }) => {
+    socket.on('single-chat/send-message', (params: { message: string, target_socket_id: string }, acknowledgment: (result: any) => void) => {
         // Log the received message
         console.log(`Received message from ${socket.id}: ${params?.message}`);
-
-        // Send the message to the specified client using targetSocketId
-        socket.to(params?.target_socket_id).emit('single-chat/receive-message', params?.message);
+        
+        if (acknowledgment) {
+            acknowledgment({ message: `message received` });
+        }
     });
 };
 
