077177771380013cfb3214061beacc6570b4808c
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-17 15:06:02 +0600

    refactor(room): updated

---
+++ socketio-ts/server-backend/src/model/app/groupchat.model.ts
@@ -0,0 +1,52 @@
+import { Schema, model, Document, Types } from 'mongoose';
+
+enum IsActiveEnum {
+    Inactive = 0,
+    Active = 1,
+}
+
+interface IGroupChat extends Document {
+    name: string;
+    admins: Types.ObjectId[]; // Replace with the actual type for admins
+    admins_history: Types.ObjectId[]; // Replace with the actual type for admins_history
+    users: Types.ObjectId[]; // Replace with the actual type for users
+    users_history: Types.ObjectId[]; // Replace with the actual type for users_history
+
+    is_active: IsActiveEnum;
+    app_id?: Types.ObjectId;
+
+    created_by?: Types.ObjectId;
+    created_at?: Date;
+}
+
+const GroupChatSchema = new Schema<IGroupChat>({
+    name: { type: String, required: true },
+    admins: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Updated type to Schema.Types.ObjectId
+    admins_history: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Updated type to Schema.Types.ObjectId
+    users: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Updated type to Schema.Types.ObjectId
+    users_history: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Updated type to Schema.Types.ObjectId
+
+    is_active: {
+        type: Number,
+        enum: [IsActiveEnum.Inactive, IsActiveEnum.Active],
+        default: IsActiveEnum.Active,
+    },
+    app_id: { type: Schema.Types.ObjectId, ref: 'AppModel' }, // Reference to App model
+
+    created_by: { type: Schema.Types.ObjectId, ref: 'User' },
+    created_at: { type: Date, default: () => Date.now() },
+});
+
+// Add a virtual property to the AppSchema
+GroupChatSchema.virtual('app', {
+    type: 'ObjectId',
+    ref: 'AppModel',
+    foreignField: '_id',
+    localField: 'app_id',
+    justOne: true
+});
+
+
+const GroupChat = model<IGroupChat>('GroupChat', GroupChatSchema, 'namespace');
+
+export { IGroupChat, GroupChat };
