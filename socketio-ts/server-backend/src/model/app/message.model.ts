f7a498fbf3c475687ddab6ada20fc1b50c5e3149
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-17 19:49:52 +0600

    feat(group-chat): adding

--- socketio-ts/server-backend/src/model/app/message.model.ts
+++ socketio-ts/server-backend/src/model/app/message.model.ts
@@ -1,22 +1,37 @@
 import { Schema, model, Document, Types } from 'mongoose';
 
+enum IsReadEnum {
+    IsRead = 1,
+    NotReadYet = 0,
+}
+
+
 interface IMessage extends Document {
     name: string;
 
     app_id: Types.ObjectId;
     namespace_id: Types.ObjectId;
-
+    group_chat_id?: Types.ObjectId;
+    is_read?: IsReadEnum;
+    
     created_by?: Types.ObjectId;
     created_at?: Date;
+    read_at?: Date;
 }
 
 const MessageSchema = new Schema<IMessage>({
     name: { type: String, required: true },
     app_id: { type: Schema.Types.ObjectId, ref: 'AppModel' }, // Reference to App model
     namespace_id: { type: Schema.Types.ObjectId, ref: 'Namespace' }, // Reference to App model
+    is_read: {
+        type: Number,
+        enum: [IsReadEnum.IsRead, IsReadEnum.NotReadYet],
+        default: IsReadEnum.NotReadYet,
+    },
 
     created_by: { type: Schema.Types.ObjectId, ref: 'User' },
     created_at: { type: Date, default: () => Date.now() },
+    read_at: { type: Date },
 });
 
 // Add a virtual property to the AppSchema
