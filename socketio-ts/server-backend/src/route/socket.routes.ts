91c142232f6036ca23a93ebc2670bc98fcc635e4
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-18 14:34:58 +0600

    refactor(configuration): socket configuration

--- socketio-ts/server-backend/src/route/socket.routes.ts
+++ socketio-ts/server-backend/src/route/socket.routes.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const router = Router();
 
 router.get('/socket', (req, res) => {
-    res.sendFile(path.join(__dirname, '../view/socket/socket.html'));
+    res.sendFile(path.join(__dirname, '../public/socket/socket.html'));
 });
 
 
