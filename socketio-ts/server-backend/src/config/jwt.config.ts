4edb484f5dcd4698ef3452cb185458812d7bf231
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-19 17:34:06 +0600

    feat(jwt): introducing jwt

---
+++ socketio-ts/server-backend/src/config/jwt.config.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import { config } from './index.config';
+
+export const connect = async() => {
+    await mongoose.connect(config?.mongo?.url, {
+        retryWrites:true, w: 'majority'
+    })  // ms-socketio-mongo-db-container = mongodb container name
+    .then(() => {
+        console.log('Connected to MongoDB');
+    }).catch(error => {
+        console.error('Error connecting to MongoDB:', error.message);
+    });
+}
+
+
+import * as jwt from 'jsonwebtoken';
+
+interface Payload {
+    userId: number;
+    username: string;
+}
+
+const secretKey = 'yourSecretKey'; // Replace with your secret key
+const payload: Payload = {
+    userId: 123,
+    username: 'john_doe',
+};
+
+const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+console.log('Generated JWT:', token);
