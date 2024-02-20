344f2ac7caf1b6c709f438e13288864e574557df
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-20 17:34:18 +0600

    refactor(jwt): login+registration

--- socketio-ts/server-backend/src/controller/authentication/authentication.controller.ts
+++ socketio-ts/server-backend/src/controller/authentication/authentication.controller.ts
@@ -5,44 +5,9 @@ import { HttpStatusCode } from '../../helper/httpcode.helper';
 import { unique } from '../../rule/common/unique.rule';
 import { exists } from '../../rule/common/exists.rule';
 import { User } from '../../model/authentication/user.model';
-import jwt from 'jsonwebtoken';
-import { authentication, random, JWT_SECRET, JWT_EXPIRES_IN, get_access_token, generate_access_token, JWT_EXPIRES_AT } from '../../helper/auth.helper';
+import { generate_access_token, JWT_EXPIRES_AT } from '../../helper/auth.helper';
 import bcrypt from 'bcrypt';
 
-// export const login = async (req: Request, res: Response) => {
-//     try {
-//         const { username, password } = req.body;
-
-//         // validation
-//         let validator = await exists({ 'model': User, 'field': 'username', 'value': username });
-//         if (validator.fails) {
-//             return set_response(res, null, HttpStatusCode.UnprocessableEntity, false, validator.messages, validator.errors);
-//         }
-
-//         const user = await userService.getUserByUserName(username).select('+authentication.salt +authentication.password');
-
-//         if (!user) {
-//             return set_response(res, null, HttpStatusCode.UnprocessableEntity, false, ['Not Found: User not found'], [{ field: 'username', message: 'User not found' }]);
-//         }
-
-//         const expectedHash = authentication(user.authentication.salt, password);
-
-//         if (user.authentication.password !== expectedHash) {
-//             return set_response(res, null, HttpStatusCode.Unauthorized, false, ['Unauthorized: Password does not match'], [{ field: 'password', message: 'Unauthorized: Password does not match' }]);
-//         }
-
-//         const salt = random();
-//         user.authentication.sessionToken = authentication(salt, user._id.toString());
-
-//         await user.save();
-
-//         res.cookie('SOCKET-SERVER-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/', secure: true, httpOnly: true });
-
-//         return set_response(res, user, HttpStatusCode.OK, true, ['Successfully logged in'], null);
-//     } catch (error: any) {
-//         return set_response(res, null, HttpStatusCode.InternalServerError, false, ['Internal Server Error: '], error);
-//     }
-// };
 
 export const login = async (req: Request, res: Response) => {
     try {
@@ -79,6 +44,7 @@ export const login = async (req: Request, res: Response) => {
 
 
         // Create jwt_access_token row for 1 to multiple device tokens login token (pending)
+        //  if 1 token allowed then inactive all previous tokens, if there is a limit to n then skip to n then inactive from n+1 to prev for that user
 
         res.cookie('Authorization', Authorization, { domain: 'localhost', path: '/', secure: true, httpOnly: true });
 
@@ -89,10 +55,11 @@ export const login = async (req: Request, res: Response) => {
 };
 
 
+
 export const register = async (req: Request, res: Response) => {
     try {
         // destructuring
-        const { name, email, username, password } = req.body;
+        const { name, email, username, password, is_active } = req.body;
 
         // validation
         let validator = await unique({ 'model': User, 'field': 'username', 'value': username });
@@ -100,16 +67,12 @@ export const register = async (req: Request, res: Response) => {
             return set_response(res, null, HttpStatusCode.UnprocessableEntity, false, validator.messages, validator.errors);
         }
 
-        // Create a new user
-        const salt = random();
+        const hashedPassword = await bcrypt.hash(password, 10);
         const user = await userService.createUser({
             name,
             email,
             username,
-            authentication: {
-                salt,
-                password: authentication(salt, password)
-            }
+            password: hashedPassword
         });
 
         return set_response(res, user, HttpStatusCode.Created, true, ['User created successfully'], null);
