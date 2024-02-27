685e4c07078bf1b925c76dee4e6187aced32a5f8
saifurrahman1193 <saifur.rahman1193@gmail.com>
2024-02-27 14:31:43 +0600

    feat(jwt): introducing jwt

--- socketio-ts/server-backend/src/model/app/app.model.ts
+++ socketio-ts/server-backend/src/model/app/app.model.ts
@@ -7,7 +7,10 @@ enum IsActiveEnum {
 
 interface IWebsite extends Document {
     address: string;
-    is_active: IsActiveEnum;
+    created_by?: Types.ObjectId;
+    created_at?: Date;
+    deleted_by?: Types.ObjectId;
+    deleted_at?: Date;
 }
 
 interface IApp extends Document {
@@ -21,16 +24,15 @@ interface IApp extends Document {
 
 const WebsiteSchema = new Schema<IWebsite>({
     address: { type: String, required: true },
-    is_active: {
-        type: Number,
-        enum: [IsActiveEnum.Inactive, IsActiveEnum.Active],
-        default: IsActiveEnum.Active,
-    },
+    created_by: { type: Schema.Types.ObjectId, ref: 'User' },
+    created_at: { type: Date, default: () => Date.now() },
+    deleted_by: { type: Schema.Types.ObjectId, ref: 'User' },
+    deleted_at: { type: Date, default: () => Date.now() },
 });
 
 const AppSchema = new Schema<IApp>({
     name: { type: String, required: true, unique: true },
-    password: { type: String, required: true},
+    password: { type: String, required: true },
     is_active: {
         type: Number,
         enum: [IsActiveEnum.Inactive, IsActiveEnum.Active],
